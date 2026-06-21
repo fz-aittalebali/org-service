f03c50a4197675091f4bb7d901968b5ba191fb45
fz-aittalebali <aittalebali.fatimaezahrae@gmail.com>
2026-06-21 01:49:19 +0100

    first commit

---
+++ src/main/java/org/dxc/orgservice/OrgServiceApplication.java
@@ -0,0 +1,13 @@
+package org.dxc.orgservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrgServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrgServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/dxc/orgservice/OrgServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dxc.orgservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrgServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
